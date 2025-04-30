a3f4707203ae5dfdf2aaa883d71ae4bc3becefc6
KonstantinTr202 <kostantremasov572@gmail.com>
2025-04-30 22:38:11 +0300

    Add files via upload

---
+++ Case 1.1/Case 1.1/DostupResur.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Класс, содержащий информацию о доступных ресурсах
+public class DostupResur   
+{
+        // Доступное количество руды (в тоннах)
+        static public double ruda;
+        // Доступное количество никеля (в кг)
+        static public double nikel;
+        // Доступное количество хрома (в кг)
+        static public double chrome;
+        // Доступное количество марганца (в кг)
+        static public double marganec;
+        // Доступное время работы доменной печи (в часах)
+        static public double pech;
+        // Доступное время работы конвертера (в часах)
+        static public double convert;
+        // Доступное время работы прокатного стана (в часах)
+        static public double prokat;
+}
---
+++ Case 1.1/Case 1.1/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Reader reader = new Reader();
+            File.WriteAllText(reader.WareHousePath, string.Empty);
+            double.TryParse(textBoxRuda.Text, out DostupResur.ruda);
+            double.TryParse(textBoxNikel.Text, out DostupResur.nikel);
+            double.TryParse(textBoxChrome.Text, out DostupResur.chrome);
+            double.TryParse(textBoxMarganec.Text, out DostupResur.marganec);
+            double.TryParse(textBoxPech.Text, out DostupResur.pech);
+            double.TryParse(textBoxConvert.Text, out DostupResur.convert);
+            double.TryParse(textBoxProkat.Text, out DostupResur.prokat);
+            if(DostupResur.ruda <= 0 
+              || DostupResur.nikel <= 0 
+              || DostupResur.chrome <= 0 
+              || DostupResur.marganec <= 0 
+              || DostupResur.pech <= 0 
+              || DostupResur.convert <= 0 
+              || DostupResur.prokat <= 0)
+            {
+                textBoxProverka.Text = "Ошибка, неправильно введённые ресурсы";
+            }
+            else
+            {
+                this.Hide();
+                Form2 form2 = new Form2();
+                form2.Show();
+            }
+        }
+    }
+}
