bc603f78b7507f6606695b4a6b6b9a72619e40fd
KonstantinTr202 <kostantremasov572@gmail.com>
2025-04-30 22:32:44 +0300

    Add files via upload

---
+++ WindowsFormsApp1/WindowsFormsApp1/Mark_B.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Класс, описывающий характеристики марки стали B
+public class Marka_B 
+{
+    // Потребление руды на 1 тонну стали (в тоннах)
+    public double ruda_stileB = 1.5;
+    // Потребление никеля на 1 тонну стали (в кг)
+    public double nikelB = 0;
+    // Потребление хрома на 1 тонну стали (в кг)
+    public double chromeB = 1;
+    // Потребление марганца на 1 тонну стали (в кг)
+    public double marganecB = 2;
+    // Время работы доменной печи на 1 тонну стали (в часах)
+    public double pechB = 0.4;
+    // Время работы конвертера на 1 тонну стали (в часах)
+    public double convertB = 0.3;
+    // Время работы прокатного стана на 1 тонну стали (в часах)
+    public double prokatB = 0.4;
+    // Прибыль с 1 тонны стали (в рублях)
+    public double rubB = 12000;
+    
+}
